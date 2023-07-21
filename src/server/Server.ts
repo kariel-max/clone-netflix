ff8cd303de288679e60671c01a07ece5596cbb6a
kariel-max <105429217+kariel-max@users.noreply.github.com>
2023-07-20 23:04:40 -0300

    foi

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import 'dotenv/config'
 import { router } from "./routes";
+import bodyParser from "body-parser";
+import mustache from "mustache-express";
+import path from "path";
 
 const server = express();
+
+server.set('views', path.join(__dirname, 'view'));
+server.set('view engine', 'mustache');
+server.use(bodyParser.urlencoded({extended: true}))
+server.engine('mustache', mustache());
+server.use('public', express.static(path.join(__dirname, 'public')))
+server.use(express.static('public'))
 
 server.use(router)
 
