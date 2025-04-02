be411b6259f7ed472088fa63e14e95b3648cab78
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-04-02 14:39:25 -0300

    ac

--- build/server/controllers/usuarios/payment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.payment = void 0;
-const payment = (req, res) => {
-    res.render("../../../views/passos/paymentPicker");
-};
-exports.payment = payment;
