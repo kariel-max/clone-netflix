74311b8b9255210aff1a9864171b47b663f63187
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-04-02 19:35:42 -0300

    1343

---
+++ build/server/controllers/produtos/Create.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Create = void 0;
+const Create = (req, res) => {
+    res.send('rotar create!!!');
+};
+exports.Create = Create;
---
+++ build/server/controllers/usuarios/payment.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.payment = void 0;
+const payment = (req, res) => {
+    res.render("../../../views/passos/paymentPicker");
+};
+exports.payment = payment;
