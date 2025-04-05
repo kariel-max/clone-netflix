65bdee8ee4959e97ae6f7773b7970eb0ed22f2f9
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-04-05 16:49:09 -0300

    asdgf134

--- build/src/server/routes/index.js
+++ build/src/server/routes/index.js
@@ -2,14 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-const path = require('path');
 const controllers_1 = require("./../controllers");
 const usuarios_1 = require("../controllers/usuarios");
 const main_1 = require("../controllers/main");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.get("/", (_, res) => {
-    res.sendFile(path.join(__dirname, '../../../views/home/index.html'))
+router.get("cloneflixkariel.netlify.app", (_, res) => {
+    res.render('../../../views/home/index');
 });
 router.post('/cadastro/singIn', usuarios_1.usuariosControllers.singIn);
 router.get('/cadastro/singIn', usuarios_1.usuariosControllers.singIn);
