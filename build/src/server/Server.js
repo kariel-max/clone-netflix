8a573305012a1b776960857761c7c8d37b3025fe
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-04-14 17:58:26 -0300

    swfg

--- build/src/server/Server.js
+++ build/src/server/Server.js
@@ -7,11 +7,9 @@ exports.server = void 0;
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
 const routes_1 = require("./routes");
-const body_parser_1 = __importDefault(require("body-parser"));
 const path_1 = __importDefault(require("path"));
 const server = (0, express_1.default)();
 exports.server = server;
-server.use(body_parser_1.default.urlencoded({ extended: true }));
 server.use(express_1.default.static(path_1.default.join(__dirname, 'views')))
 server.use('public', express_1.default.static(path_1.default.join(__dirname, 'public')));
 server.use(express_1.default.static('public'));
