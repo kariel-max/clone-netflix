025645fa2071a43b8a23b2394dc39fd900abd123
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-04-30 14:49:38 -0300

    bgsfdg245

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,14 @@ import sequelize from "./server/database/Sequelize/sequelize";
 server.listen( process.env.PORT, ()=> {
     console.log(' foi conectado!!!');
 });
+
+(async ()=> {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão bem-sucedida!');
+        await sequelize.sync({alter: true});
+        console.log('Banco de dados sincronizado!');
+    } catch (error) {
+        console.error('erro ao sincronizar banco:', error);
+    }
+})();
