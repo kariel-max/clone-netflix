75bd7b25d56d6caadcf4be240f1029b1200df0d5
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-05-02 18:51:40 -0300

    sfg45

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,14 +3,9 @@ import 'dotenv/config'
 import { router } from "./routes";
 import path from "path";
 import bodyParser from "body-parser";
-import cors from "cors"
 
 const server = express();
 
-server.use(cors({
-    origin: "cloneflixkariel.netlify.app",
-    methods: ['GET', 'POST']
-}))
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use('public', express.static(path.join(__dirname, 'public')))
