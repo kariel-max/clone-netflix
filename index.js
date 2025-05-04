3e62d559b29e08dc0cb5ea0c7aff9d868c9204d3
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-05-04 19:46:09 -0300

    asgfqafq

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-const sequelize_1 = __importDefault(require("./server/database/Sequelize/sequelize"));
+const Server_1 = require("./build/src/server/Server");
+const sequelize_1 = __importDefault(require("./build/src/server/database/Sequelize/sequelize"));
 const port = Number(process.env.PORT || 8080);
 Server_1.server.listen(port, '0.0.0.0', () => {
     console.log('Servidor conectado!');
