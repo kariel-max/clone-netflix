c57e71e7d38478a6554c4ffad09d866c9678c319
kariel-max <105429217+kariel-max@users.noreply.github.com>
2025-05-07 09:32:02 -0300

    sdfgv

--- build/src/server/Server.js
+++ build/src/server/Server.js
@@ -11,6 +11,12 @@ const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const server = (0, express_1.default)();
 exports.server = server;
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Ou especifique o domínio correto
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 server.use(body_parser_1.default.json());
 server.use(body_parser_1.default.urlencoded({ extended: true }));
 server.use('public', express_1.default.static(path_1.default.join(__dirname, 'public')));
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -6,6 +6,13 @@ import bodyParser from "body-parser";
 
 const server = express();
 
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Ou especifique o domínio correto
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use('public', express.static(path.join(__dirname, 'public')))
